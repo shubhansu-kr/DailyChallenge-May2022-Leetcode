ab30809f49e0370b404a5161c17a34186c71baa4
shubhansu-kr <shubhansu2021@gmail.com>
2022-05-01 17:06:53 +0530

    Solution 1

--- 00_BackSpaceStrCmp.cpp
+++ 00_BackSpaceStrCmp.cpp
@@ -12,8 +12,47 @@ using namespace std;
 
 class Solution
 {
+    // Discussion solution
+public:
+    bool backspaceCompare(string s, string t)
+    {
+        int i = s.length() - 1, j = t.length() - 1;
+        int skips = 0, skipt = 0;
+        while (i >= 0 || j >= 0)
+        {
+            while (i >= 0)
+            {
+                if (s[i] == '#')
+                    skips++, i--;
+                else if (skips > 0)
+                    skips--, i--;
+                else
+                    break;
+            }
+            while (j >= 0)
+            {
+                if (t[j] == '#')
+                    skipt++, j--;
+                else if (skipt > 0)
+                    skipt--, j--;
+                else
+                    break;
+            }
+            if (i >= 0 && j >= 0 && s[i] != t[j])
+                return false;
+            // One string gets empty, unequal length string also result in false
+            if (i >= 0 ^ j >= 0)
+                return false;
+            i--, j--;
+        }
+        return true;
+    }
+};
+
+class Solution
+{
     // Try solving using stack
-    // Wrong solution 
+    // Wrong solution
 public:
     void edit(string &s)
     {
