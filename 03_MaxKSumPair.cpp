8e364b49b26eb3a77b15cf7a93d7df3b0399d369
shubhansu-kr <shubhansu2021@gmail.com>
2022-05-04 10:47:23 +0530

    TLE

---
+++ 03_MaxKSumPair.cpp
@@ -0,0 +1,52 @@
+// https://leetcode.com/problems/max-number-of-k-sum-pairs/
+
+// You are given an integer array nums and an integer k.
+
+// In one operation, you can pick two numbers from the array
+// whose sum equals k and remove them from the array.
+
+// Return the maximum number of operations you can perform
+// on the array.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    // Brute force : Time Limit Exceeded 
+public:
+    int maxOperations(vector<int> &nums, int k)
+    {
+        int count = 0;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] >= k)
+                break;
+            if (nums[i])
+            {
+                for (int j = i + 1; j < nums.size(); j++)
+                {
+                    if (nums[j])
+                    {
+                        int s = nums[i] + nums[j];
+                        if (nums[j] >= k || s > k)
+                            break;
+                        if (s == k)
+                        {
+                            ++count;
+                            nums[j] = 0;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+};
+
+int main()
+{
+    return 0;
+}
