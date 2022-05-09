5ab324c5cb3fa6b26702899c7d18ae0ae3ae447d
shubhansu-kr <shubhansu2021@gmail.com>
2022-05-09 10:22:07 +0530

    All possible keypad patterns

---
+++ 08_MobilePattern.cpp
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    const vector<string> pad = {
+        "", "", "abc", "def", "ghi", "jkl",
+        "mno", "pqrs", "tuv", "wxyz"};
+
+    vector<string> letterCombinations(string digits)
+    {
+        if (digits.empty())
+            return {};
+        vector<string> result;
+        result.push_back("");
+
+        for (auto digit : digits)
+        {
+            vector<string> tmp;
+            for (auto candidate : pad[digit - '0'])
+            {
+                for (auto s : result)
+                {
+                    tmp.push_back(s + candidate);
+                }
+            }
+            result.swap(tmp);
+        }
+        return result;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
