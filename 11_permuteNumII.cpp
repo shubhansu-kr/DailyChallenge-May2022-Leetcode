357029a48a1f65dd82499d56b4020c2450d184f6
shubhansu-kr <shubhansu2021@gmail.com>
2022-05-12 10:51:33 +0530

    Permutation 11

---
+++ 11_permuteNumII.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/permutations-ii/
+
+// Given a collection of numbers, nums, that might contain duplicates, 
+// return all possible unique permutations in any order.
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    void recursion(vector<int> num, int i, int j, vector<vector<int> > &res) {
+        if (i == j-1) {
+            res.push_back(num);
+            return;
+        }
+        for (int k = i; k < j; k++) {
+            if (i != k && num[i] == num[k]) continue;
+            swap(num[i], num[k]);
+            recursion(num, i+1, j, res);
+        }
+    }
+    vector<vector<int> > permuteUnique(vector<int> &num) {
+        sort(num.begin(), num.end());
+        vector<vector<int> >res;
+        recursion(num, 0, num.size(), res);
+        return res;
+    }
+};
+int main () {
+    
+    return 0;
+}
