8a308e129d7fbc49e63e51878b3b16cf1ae05fbf
shubhansu-kr <shubhansu2021@gmail.com>
2022-05-29 12:51:08 +0530

    Max Length product

--- 28_MaxLength.cpp
+++ 28_MaxLength.cpp
@@ -9,7 +9,26 @@ using namespace std;
 
 class Solution
 {
-    // Brute Force : TLE 
+public:
+    int maxProduct(vector<string> &words)
+    {
+        vector<int> mask(words.size());
+        int result = 0;
+        for (int i = 0; i < words.size(); ++i)
+        {
+            for (char c : words[i])
+                mask[i] |= 1 << (c - 'a');
+            for (int j = 0; j < i; ++j)
+                if (!(mask[i] & mask[j]))
+                    result = max(result, int(words[i].size() * words[j].size()));
+        }
+        return result;
+    }
+};
+
+class Solution
+{
+    // Brute Force : TLE
 public:
     bool isValid(string a, string b)
     {
