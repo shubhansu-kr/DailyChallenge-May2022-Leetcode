e58d2866487fdfd24dd47b4f8418797a207dff2a
shubhansu-kr <shubhansu2021@gmail.com>
2022-05-30 13:33:21 +0530

    Divide integer

--- 29_DivideIntegers.cpp
+++ 29_DivideIntegers.cpp
@@ -20,6 +20,33 @@ using namespace std;
 
 class Solution
 {
+    // Discussion solution
+public:
+    int divide(int dividend, int divisor)
+    {
+        if (dividend == INT_MIN && divisor == -1)
+        {
+            return INT_MAX;
+        }
+        long dvd = labs(dividend), dvs = labs(divisor), ans = 0;
+        int sign = dividend > 0 ^ divisor > 0 ? -1 : 1;
+        while (dvd >= dvs)
+        {
+            long temp = dvs, m = 1;
+            while (temp << 1 <= dvd)
+            {
+                temp <<= 1;
+                m <<= 1;
+            }
+            dvd -= temp;
+            ans += m;
+        }
+        return sign * ans;
+    }
+};
+
+class Solution
+{
     // Runtime error
 public:
     int divide(int dividend, int divisor)
